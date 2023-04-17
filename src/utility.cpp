244fed415fec3fe915c96fdaf0eed6d93b33b432
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-17 13:23:31 +0300

    implement initBinaryFile method

---
+++ src/utility.cpp
@@ -0,0 +1,12 @@
+#include "utility.h"
+
+void Utility::initBinaryFile(const std::string& binaryFileName, const Employee* employees, const size_t& size)
+{
+    std::fstream fout(binaryFileName.c_str(), std::ios::binary | std::ios::out);
+    fout.seekp(0);
+    fout.write(reinterpret_cast<const char*>(&size), sizeof(size_t));
+    for (size_t i = 0; i < size; ++i)
+    {
+        fout.write(reinterpret_cast<const char*>(employees), sizeof(employees));
+    }
+}
