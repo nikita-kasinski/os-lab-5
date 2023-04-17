9129202f4922d91a111450ab31d4e4d681c9bd61
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-17 13:30:07 +0300

    declare getRecord method

--- include/utility.h
+++ include/utility.h
@@ -12,10 +12,13 @@ class Utility
     static constexpr auto maxStreamSize = std::numeric_limits<std::streamsize>::max();
     static constexpr size_t overallOffset = sizeof(size_t);
     static constexpr size_t singleOffset = sizeof(Employee);
-    
+
 public:
 
     // initializes binary files with given array of employees
     static void initBinaryFile(const std::string &binaryFileName, const Employee *employees, const size_t &size);
 
+    // gets record by given id and puts it into employee, return false in case there is no employee under such id
+    static bool getRecord(std::istream& f, const size_t& id, Employee& employee);
+    
 };
