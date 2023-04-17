39734de97a8fe89f4013718a7e1942809d189117
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-17 13:23:18 +0300

    define offset variables

--- include/utility.h
+++ include/utility.h
@@ -10,10 +10,12 @@
 class Utility
 {
     static constexpr auto maxStreamSize = std::numeric_limits<std::streamsize>::max();
-
+    static constexpr size_t overallOffset = sizeof(size_t);
+    static constexpr size_t singleOffset = sizeof(Employee);
+    
 public:
 
     // initializes binary files with given array of employees
     static void initBinaryFile(const std::string &binaryFileName, const Employee *employees, const size_t &size);
-    
+
 };
