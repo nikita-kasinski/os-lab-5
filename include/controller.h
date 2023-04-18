0e63b38805b429dbe814db6472ef68d98b8f5043
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 15:08:19 +0300

    declare setRecord method

--- include/controller.h
+++ include/controller.h
@@ -15,11 +15,14 @@ class Controller
 private:
     Model model;
     std::map<int, std::size_t> idToRecordId;
-    
+
 public:
     // contructs utility object to manage binary file filled with given array of employees
     Controller(const std::string &binaryFileName, const Employee *employees, const std::size_t &size, bool &ok);
 
     // gets record by given id and puts it into employee variable, return false in case there is no employee under such id
     bool getRecord(const std::size_t &id, Employee &employee) const;
+
+    // sets record by oldId to employee
+    bool setRecord(const std::size_t &oldId, const Employee &employee) const;
 };
