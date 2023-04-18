6d8c14a92c854b6291b540a7f230396496d40deb
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 16:01:53 +0300

    add another unit test for readRecord method

--- test/test.cc
+++ test/test.cc
@@ -4,11 +4,29 @@
 
 TEST(TestModelReadRecord, TestOnlyOneRecord)
 {
-    constexpr size_t size = 1;
+    constexpr std::size_t size = 1;
     Employee firstEmployee = {1, "vasya", 5.2};
     Employee employees[size] = {firstEmployee};
     const std::string binaryFileName = "testbinary";
     Model model(binaryFileName);
     model.writeBinaryFile(employees, size);
     EXPECT_EQ(firstEmployee, model.readRecord(0));
+}
+
+TEST(TestModelReadRecord, TestSeveralRecords)
+{
+    constexpr std::size_t size = 3;
+    Employee firstEmployee = {1, "vasya", 4.8};
+    Employee secondEmployee = {2, "petya", 5.2};
+    Employee thirdEmployee = {3, "kostya", 7};
+    Employee employees[size] = {firstEmployee, secondEmployee, thirdEmployee};
+    const std::string binaryFileName = "testbinary";
+    Model model(binaryFileName);
+    model.writeBinaryFile(employees, size);
+    EXPECT_EQ(firstEmployee, model.readRecord(0));
+    EXPECT_EQ(secondEmployee, model.readRecord(1));
+    EXPECT_EQ(thirdEmployee, model.readRecord(2));
+    EXPECT_EQ(secondEmployee, model.readRecord(1));
+    EXPECT_EQ(firstEmployee, model.readRecord(0));
+    EXPECT_EQ(thirdEmployee, model.readRecord(2));
 }
