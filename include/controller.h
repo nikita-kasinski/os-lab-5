23532e9a1e071a7e707daabd7dc411c5e79088d1
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 15:12:29 +0300

    improve comments on setRecord

--- include/controller.h
+++ include/controller.h
@@ -20,9 +20,9 @@ public:
     // contructs utility object to manage binary file filled with given array of employees
     Controller(const std::string &binaryFileName, const Employee *employees, const std::size_t &size, bool &ok);
 
-    // gets record by given id and puts it into employee variable, return false in case there is no employee under such id
+    // gets record by given id and puts it into employee variable, returns false in case there is no employee under such id
     bool getRecord(const std::size_t &id, Employee &employee) const;
 
-    // sets record by oldId to employee
+    // sets record by oldId to employee, returns false in case there is no employee under oldId or newId(employee.id) is taken
     bool setRecord(const std::size_t &oldId, const Employee &employee) const;
 };
