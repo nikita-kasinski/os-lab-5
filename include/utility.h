0d85ccbd7d0a041a2f9e16841908b1a2e82b541a
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 09:08:49 +0300

    make utility class non static via making initBinaryFile constructor

--- include/utility.h
+++ include/utility.h
@@ -5,20 +5,22 @@
 
 #include <limits>
 #include <fstream>
+#include <map>
 #include "employee.h"
 
 class Utility
 {
+private:
     static constexpr auto maxStreamSize = std::numeric_limits<std::streamsize>::max();
     static constexpr size_t overallOffset = sizeof(size_t);
     static constexpr size_t singleOffset = sizeof(Employee);
-
+    std::map<int, size_t> idToRecordId;
 public:
 
-    // initializes binary files with given array of employees
-    static void initBinaryFile(const std::string &binaryFileName, const Employee *employees, const size_t &size);
+    // contructs utility object to manage binary file filled with given array of employees
+    Utility(const std::string &binaryFileName, const Employee *employees, const size_t &size);
 
     // gets record by given id and puts it into employee, return false in case there is no employee under such id
-    static bool getRecord(std::istream& f, const size_t& id, Employee& employee);
+    bool getRecord(std::istream& f, const size_t& id, Employee& employee);
     
 };
