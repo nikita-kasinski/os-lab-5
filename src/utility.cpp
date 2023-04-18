84089920be655dda8b11466d914020febf0933b2
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 18:00:54 +0300

    implement safeUnsignedIntegerInput method

--- src/utility.cpp
+++ src/utility.cpp
@@ -52,3 +52,31 @@ void Utility::printEmployee(std::ostream &out, const Employee &employee)
         << std::setw(Utility::hoursWidth) << employee.hours
         << "\n";
 }
+
+size_t Utility::safeUnsignedIntegerInput(std::istream &in, std::ostream &out, const std::string &inputPrompt, const std::string &inputFailedPrompt)
+{
+    constexpr auto maxStreamSize = std::numeric_limits<std::streamsize>::max();
+    int tempAns = -1;
+    bool failed = false;
+
+    while (tempAns <= 0)
+    {
+        if (failed)
+        {
+            out << inputFailedPrompt;
+        }
+        else
+        {
+            failed = true;
+        }
+        out << inputPrompt;
+        in >> tempAns;
+        if (!in.good())
+        {
+            in.clear();
+            in.ignore(maxStreamSize, '\n');
+            tempAns = -1;
+        }
+    }
+    return static_cast<size_t>(tempAns);
+}
