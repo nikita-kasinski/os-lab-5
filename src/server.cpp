7f6f6e57c6972e57ee628ee604ed1042f15d1cf5
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-18 18:09:39 +0300

    implement employees and binary file name input

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,33 @@
+//
+// Created by Nikita Kasinski
+//
+
 #include <iostream>
+#include "utility.h"
+#include "controller.h"
 
 int main()
 {
-    std::cout << "Hello world\n";
+    // retrieving number of employees
+    size_t size = Utility::safeUnsignedIntegerInput(
+        std::cin,
+        std::cout, 
+        "Enter number of employees: ",
+        "Value must be positive integer\n");
+
+    // retrieving employees
+    Employee *employees = new Employee[size];
+    for (size_t i = 0; i < size; ++i)
+    {
+        employees[i] = Utility::readEmployee(std::cin, std::cout);
+    }
+
+    // retrieving binary file name
+    std::string binaryFileName;
+    std::cin >> binaryFileName;
+
+    
+    // freeing memory
+    delete[] employees;
     return 0;
 }
