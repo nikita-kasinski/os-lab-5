768642da626e53b24044259e5423430af9d4b15d
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-19 09:44:55 +0300

    declare idToRecordId method

--- include/controller.h
+++ include/controller.h
@@ -14,7 +14,7 @@ class Controller
 {
 private:
     Model model;
-    std::map<int, std::size_t> idToRecordId;
+    std::map<int, std::size_t> _idToRecordId;
 
 public:
     // contructs controller object to manage binary file filled with given array of employees
@@ -25,6 +25,9 @@ public:
 
     // sets record by oldId to employee, returns false in case there is no employee under oldId or newId(employee.id) is taken
     bool setRecord(const int &oldId, const Employee &employee);
+
+    // maps id into recordId. Puts answer to recordId. Returns false if there is no such id
+    bool idToRecordId(const int& id, size_t& recordId) const;
 
     // get all records from a file. Array size is put into size. Array must be freed
     Employee* getAllRecords(size_t& size) const;
