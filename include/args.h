4536eebb4319589c8c6b68960d053b30a90dd45f
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-19 09:37:11 +0300

    add controller and critical section fields to thread args

--- include/args.h
+++ include/args.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <cstdlib>
+#include "controller.h"
 
 struct ThreadArgs
 {
     std::size_t id;
     std::size_t numberOfClients;
     std::size_t numberOfRecords;
+    Controller *ctrl;
+    CRITICAL_SECTION *iocs;
 };
