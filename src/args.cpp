5b4cce37271961ba993a3d30f2843424cb7e91a6
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-02 21:12:43 +0300

    implement constructor and getters for ClientHandlerArgs

--- src/args.cpp
+++ src/args.cpp
@@ -1,1 +1,40 @@
 #include "args.h"
+
+ClientHandlerArgs::ClientHandlerArgs(
+    std::size_t id,
+    std::size_t numberOfClients,
+    std::size_t numberOfRecords,
+    const std::shared_ptr<Controller> &ctrl,
+    const std::shared_ptr<CRITICAL_SECTION> &iocs)
+{
+    _id = id;
+    _numberOfClients = numberOfClients;
+    _numberOfRecords = numberOfRecords;
+    _ctrl = ctrl;
+    _iocs = iocs;
+}
+
+std::size_t ClientHandlerArgs::getId() const
+{
+    return _id;
+}
+
+std::size_t ClientHandlerArgs::getNumberOfClients() const
+{
+    return _numberOfClients;
+}
+
+std::size_t ClientHandlerArgs::getNumberOfRecords() const
+{
+    return _numberOfRecords;
+}
+
+std::shared_ptr<Controller> ClientHandlerArgs::getController() const
+{
+    return _ctrl;
+}
+
+std::shared_ptr<CRITICAL_SECTION> ClientHandlerArgs::getCriticalSection() const
+{
+    return _iocs;
+}
