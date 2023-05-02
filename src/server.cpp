1179d1b70378ee8265d401f4eb0b1de6196f4460
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-02 21:31:59 +0300

    make server use clientHandler from separate file

--- src/server.cpp
+++ src/server.cpp
@@ -11,6 +11,7 @@
 #include "controller.h"
 #include "args.h"
 #include "protocol.h"
+#include "clientHandler.h"
 
 BOOL StartClient()
 {
@@ -37,251 +38,6 @@ BOOL StartClient()
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
     return result;
-}
-
-DWORD WINAPI InteractWithClientThread(LPVOID _args)
-{
-    const std::string pipeName = R"(\\.\pipe\os-lab5-pipe)";
-    ThreadArgs args = *(ThreadArgs *)_args;
-    std::size_t threadId = args.id;
-    std::size_t numberOfClients = args.numberOfClients;
-    Controller *ctrl = args.ctrl;
-    CRITICAL_SECTION *iocs = args.iocs;
-
-    // creating named pipe
-    HANDLE pipe = CreateNamedPipeA(
-        pipeName.c_str(),
-        PIPE_ACCESS_DUPLEX,
-        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-        numberOfClients,
-        0,
-        0,
-        INFINITE,
-        NULL);
-
-    if (pipe == INVALID_HANDLE_VALUE)
-    {
-        // pipe didn't open
-        EnterCriticalSection(iocs);
-        std::cerr << "Thread " << threadId << " cannot open a pipe: " << GetLastError() << "\n";
-        LeaveCriticalSection(iocs);
-        return 2;
-    }
-
-    if (!ConnectNamedPipe(pipe, NULL))
-    {
-        CloseHandle(pipe);
-        EnterCriticalSection(iocs);
-        std::cerr << "Thread " << threadId << ". Connection error: " << GetLastError() << "\n";
-        LeaveCriticalSection(iocs);
-        return 3;
-    }
-
-    while (true)
-    {
-        DWORD bytes;
-        char request;
-
-        EnterCriticalSection(iocs);
-        std::cout << "Thread " << threadId << " is working\n";
-        LeaveCriticalSection(iocs);
-
-        ReadFile(pipe, &request, sizeof(char), &bytes, NULL);
-
-        if (Protocol::QUIT == request)
-        {
-            DisconnectNamedPipe(pipe);
-            break;
-        }
-        else if (Protocol::READ == request)
-        {
-            // reading record
-
-            int key; // employee id
-            ReadFile(pipe, reinterpret_cast<char *>(&key), sizeof(int), &bytes, NULL);
-
-            EnterCriticalSection(iocs);
-            std::cout << "Thread " << threadId << " received key to read: " << key << "\n";
-            LeaveCriticalSection(iocs);
-
-            size_t recordId;
-            bool keyExists = ctrl->idToRecordId(key, recordId); // record id
-
-            if (keyExists)
-            {
-                HANDLE notBeingModifiedEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, Utility::getWriteEventName(recordId).c_str());
-                HANDLE notBeingReadByClientEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, Utility::getReadEventName(recordId, threadId).c_str());
-
-                EnterCriticalSection(iocs);
-                std::cout << "Thread " << threadId << " is waiting for record with key " << key << " to become available\n";
-                LeaveCriticalSection(iocs);
-
-                WaitForSingleObject(notBeingModifiedEvent, INFINITE); // waiting for record to become available
-
-                EnterCriticalSection(iocs);
-                std::cout << "Thread " << threadId << " access granted\n";
-                LeaveCriticalSection(iocs);
-
-                // marking record as being read
-                ResetEvent(notBeingReadByClientEvent);
-
-                // reading record and sending to client
-                Employee employeeRead;
-                if (ctrl->getRecord(key, employeeRead))
-                {
-
-                    EnterCriticalSection(iocs);
-                    std::cout << "Thread " << threadId << " has read employee and has sent it\n";
-                    LeaveCriticalSection(iocs);
-
-                    WriteFile(pipe, &Protocol::SUCCESS, Protocol::SIZE, &bytes, NULL);
-                    WriteFile(pipe, reinterpret_cast<char *>(&employeeRead), sizeof(Employee), &bytes, NULL);
-
-                    char response;
-
-                    // waiting for client to finish access to record
-                    EnterCriticalSection(iocs);
-                    std::cout << "Thread " << threadId << " is waiting for client to stop reading record\n";
-                    LeaveCriticalSection(iocs);
-
-                    ReadFile(pipe, &response, Protocol::SIZE, &bytes, NULL);
-
-                    if (Protocol::FINISH != response)
-                    {
-                        EnterCriticalSection(iocs);
-                        std::cerr << "Protocol violation on stopping reading a record\n. Exit thread";
-                        LeaveCriticalSection(iocs);
-                        ExitThread(2);
-                    }
-
-                    EnterCriticalSection(iocs);
-                    std::cout << "Thread " << threadId << " has stopped reading record\n";
-                    LeaveCriticalSection(iocs);
-                }
-                else
-                {
-                    WriteFile(pipe, &Protocol::FAILURE, Protocol::SIZE, &bytes, NULL);
-                }
-
-                // marking record as not being read
-                SetEvent(notBeingReadByClientEvent);
-                CloseHandle(notBeingModifiedEvent);
-                CloseHandle(notBeingReadByClientEvent);
-            }
-            else
-            {
-                WriteFile(pipe, &Protocol::FAILURE, Protocol::SIZE, &bytes, NULL);
-            }
-        }
-        else if (Protocol::MODIFY == request)
-        {
-            // modifying record
-
-            // reading key
-            int key;
-            ReadFile(pipe, reinterpret_cast<char *>(&key), sizeof(int), &bytes, NULL);
-
-            // trying to get recordId by id
-            size_t recordId;
-            bool keyExists = ctrl->idToRecordId(key, recordId);
-
-            if (keyExists)
-            {
-                // record exists
-
-                // opening events
-                HANDLE notBeingModifiedEvent = OpenEventA(EVENT_ALL_ACCESS, FALSE, Utility::getWriteEventName(recordId).c_str());
-                HANDLE *notBeingReadEvents = new HANDLE[numberOfClients];
-                for (size_t i = 0; i < numberOfClients; ++i)
-                {
-                    notBeingReadEvents[i] = OpenEventA(EVENT_ALL_ACCESS, FALSE, Utility::getReadEventName(recordId, i).c_str());
-                }
-
-                EnterCriticalSection(iocs);
-                std::cout << "Thread " << threadId << " is waiting for record " << recordId << " to become available for modifying\n";
-                LeaveCriticalSection(iocs);
-
-                // waiting for all clients to stop reading record
-                WaitForMultipleObjects(numberOfClients, notBeingReadEvents, TRUE, INFINITE);
-                WaitForSingleObject(notBeingModifiedEvent, INFINITE);
-
-                // marking record as being modified
-                ResetEvent(notBeingModifiedEvent);
-
-                EnterCriticalSection(iocs);
-                std::cout << "Thread " << threadId << " access granted\n";
-                LeaveCriticalSection(iocs);
-
-                // writing success response
-                WriteFile(pipe, &Protocol::SUCCESS, Protocol::SIZE, &bytes, NULL);
-
-                // reading record
-                Employee employeeRead;
-                if (ctrl->getRecord(key, employeeRead))
-                {
-
-                    // writing record
-                    WriteFile(pipe, reinterpret_cast<char *>(&employeeRead), sizeof(Employee), &bytes, NULL);
-
-                    // reading new record
-                    ReadFile(pipe, reinterpret_cast<char *>(&employeeRead), sizeof(Employee), &bytes, NULL);
-
-                    if (ctrl->setRecord(key, employeeRead))
-                    {
-                        WriteFile(pipe, &Protocol::SUCCESS, Protocol::SIZE, &bytes, NULL);
-
-                        char response;
-                        ReadFile(pipe, &response, Protocol::SIZE, &bytes, NULL);
-
-                        if (Protocol::FINISH != response)
-                        {
-                            EnterCriticalSection(iocs);
-                            std::cout << "Thread " << threadId << " protocol violation. Abort\n";
-                            ExitThread(4);
-                        }
-                    }
-                    else
-                    {
-                        WriteFile(pipe, &Protocol::FAILURE, Protocol::SIZE, &bytes, NULL);
-                    }
-                }
-                else
-                {
-                    WriteFile(pipe, &Protocol::FAILURE, Protocol::SIZE, &bytes, NULL);
-                }
-
-                // marking record as not being modified
-                SetEvent(notBeingModifiedEvent);
-                CloseHandle(notBeingModifiedEvent);
-                for (size_t i = 0; i < numberOfClients; ++i)
-                {
-                    CloseHandle(notBeingReadEvents[i]);
-                }
-                delete[] notBeingReadEvents;
-            }
-            else
-            {
-                // writing failure response
-                WriteFile(pipe, &Protocol::FAILURE, Protocol::SIZE, &bytes, NULL);
-            }
-        }
-        else
-        {
-            EnterCriticalSection(iocs);
-            std::cerr << "Protocol violation. Exit thread\n";
-            LeaveCriticalSection(iocs);
-
-            ExitThread(3);
-        }
-    }
-
-    EnterCriticalSection(iocs);
-    std::cout << "Thread " << threadId << " ended\n";
-    LeaveCriticalSection(iocs);
-
-    CloseHandle(pipe);
-
-    return 0;
 }
 
 int main()
@@ -359,16 +115,12 @@ int main()
 
     // starting client interaction
     std::vector<HANDLE> threads(numberOfClients);
-    std::vector<ThreadArgs> args(numberOfClients);
+    std::vector<ClientHandlerArgs> args(numberOfClients);
     for (size_t i = 0; i < numberOfClients; ++i)
     {
         DWORD thread_id;
-        args[i].id = i;
-        args[i].numberOfClients = numberOfClients;
-        args[i].numberOfRecords = numberOfRecords;
-        args[i].ctrl = ctrl;
-        args[i].iocs = iocs;
-        threads[i] = CreateThread(NULL, 0, InteractWithClientThread, (LPVOID *)(&args[i]), 0, &thread_id);
+        args[i] = ClientHandlerArgs(i, numberOfClients, numberOfRecords, ctrl, iocs);
+        threads[i] = CreateThread(NULL, 0, ClientHandler, (LPVOID *)(&args[i]), 0, &thread_id);
     }
 
     // starting clients
@@ -384,7 +136,7 @@ int main()
     std::vector<Employee> employeesFromModifiedFile = ctrl->getAllRecords();
     Utility::printEmployees(std::cout, employeesFromModifiedFile);
 
-    // freeing memory
+    // closing handles
     DeleteCriticalSection(iocs.get());
     for (size_t i = 0; i < numberOfRecords; ++i)
     {
