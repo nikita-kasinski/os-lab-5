4c2aa36926f1130410134e79030c3f0873970a66
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-03 15:14:21 +0300

    move all functionality into class, add make_shared_methods

--- include/smart_winapi.h
+++ include/smart_winapi.h
@@ -6,15 +6,26 @@
 #include <windows.h>
 #include <memory>
 
-struct CriticalSectionDeleter
+class SmartWinapi
 {
-    void operator()(CRITICAL_SECTION *cs) const;
-};
+private:
+    struct CriticalSectionDeleter
+    {
+        void operator()(CRITICAL_SECTION *cs) const;
+    };
 
-struct HandleCloser
-{
-    void operator()(HANDLE *handle) const;
-};
+    struct HandleCloser
+    {
+        void operator()(HANDLE *handle) const;
+    };
 
-std::unique_ptr<CRITICAL_SECTION, CriticalSectionDeleter> make_unique_cs();
-std::unique_ptr<HANDLE, HandleCloser> make_unique_handle(HANDLE source);
+    static CRITICAL_SECTION *createCriticalSection();
+    static HANDLE *createHandle(HANDLE source);
+
+public:
+    static std::unique_ptr<CRITICAL_SECTION, CriticalSectionDeleter> make_unique_cs();
+    static std::unique_ptr<HANDLE, HandleCloser> make_unique_handle(HANDLE source);
+
+    static std::shared_ptr<CRITICAL_SECTION> make_shared_cs();
+    static std::shared_ptr<HANDLE> make_shared_handle(HANDLE source);
+};
