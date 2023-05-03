fc7b7fb4d5e844ed3517dd929e65fb0d9135999e
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-03 10:06:27 +0300

    add array access critical section and recordAccessReadCount into ClientHandlerArgs

--- include/args.h
+++ include/args.h
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <windows.h>
 #include <memory>
+#include <vector>
 #include "controller.h"
 
 // data class for providing thread function ClientHandler with arguments
@@ -15,19 +16,25 @@ private:
     std::size_t _id;
     std::size_t _numberOfClients;
     std::size_t _numberOfRecords;
+    std::shared_ptr<std::vector<std::size_t>> _recordAccessReadCount;
     std::shared_ptr<Controller> _ctrl;
     std::shared_ptr<CRITICAL_SECTION> _iocs;
+    std::shared_ptr<CRITICAL_SECTION> _acs;
 public:
     ClientHandlerArgs();
     ClientHandlerArgs(
         std::size_t id, 
         std::size_t numberOfClients, 
         std::size_t numberOfRecords, 
+        const std::shared_ptr<std::vector<std::size_t>>& recordAccessReadCount,
         const std::shared_ptr<Controller>& ctrl, 
-        const std::shared_ptr<CRITICAL_SECTION>& iocs);
+        const std::shared_ptr<CRITICAL_SECTION>& iocs,
+        const std::shared_ptr<CRITICAL_SECTION>& acs);
     std::size_t getId() const;
     std::size_t getNumberOfClients() const;
     std::size_t getNumberOfRecords() const;
+    std::shared_ptr<std::vector<std::size_t>> getRecordAccessReadCount() const;
     std::shared_ptr<Controller> getController() const;
-    std::shared_ptr<CRITICAL_SECTION> getCriticalSection() const;
+    std::shared_ptr<CRITICAL_SECTION> getIOCriticalSection() const;
+    std::shared_ptr<CRITICAL_SECTION> getArrayCriticalSection() const;
 };
--- src/args.cpp
+++ src/args.cpp
@@ -11,20 +11,25 @@ ClientHandlerArgs::ClientHandlerArgs()
     _numberOfRecords = 0;
     _ctrl = std::shared_ptr<Controller>();
     _iocs = std::shared_ptr<CRITICAL_SECTION>();
+    _acs = std::shared_ptr<CRITICAL_SECTION>();
 }
 
 ClientHandlerArgs::ClientHandlerArgs(
     std::size_t id,
     std::size_t numberOfClients,
     std::size_t numberOfRecords,
+    const std::shared_ptr<std::vector<std::size_t>> &recordAccessReadCount,
     const std::shared_ptr<Controller> &ctrl,
-    const std::shared_ptr<CRITICAL_SECTION> &iocs)
+    const std::shared_ptr<CRITICAL_SECTION> &iocs,
+    const std::shared_ptr<CRITICAL_SECTION> &acs)
 {
     _id = id;
     _numberOfClients = numberOfClients;
     _numberOfRecords = numberOfRecords;
+    _recordAccessReadCount = recordAccessReadCount;
     _ctrl = ctrl;
     _iocs = iocs;
+    _acs = acs;
 }
 
 std::size_t ClientHandlerArgs::getId() const
@@ -47,7 +52,17 @@ std::shared_ptr<Controller> ClientHandlerArgs::getController() const
     return _ctrl;
 }
 
-std::shared_ptr<CRITICAL_SECTION> ClientHandlerArgs::getCriticalSection() const
+std::shared_ptr<CRITICAL_SECTION> ClientHandlerArgs::getIOCriticalSection() const
 {
     return _iocs;
+}
+
+std::shared_ptr<CRITICAL_SECTION> ClientHandlerArgs::getArrayCriticalSection() const
+{
+    return _acs;
+}
+
+std::shared_ptr<std::vector<std::size_t>> ClientHandlerArgs::getRecordAccessReadCount() const
+{
+    return _recordAccessReadCount;
 }
