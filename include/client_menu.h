aebb30619416e69a613e8c14fa6f8ac6ac356bcb
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-06 11:04:09 +0300

    add pipe out and in fields to ClientMenu

--- include/client_menu.h
+++ include/client_menu.h
@@ -3,20 +3,25 @@
 //
 #pragma once
 
+#include <iostream>
 #include "menu.h"
 
-class ClientOptionExit: public MenuOption
+class ClientOptionExit : public MenuOption
 {
 public:
     [[nodiscard]] ResultCode execute() const override;
     bool isQuitOption() const override;
 };
 
-class ClientMenu: public Menu
+class ClientMenu : public Menu
 {
-protected:
-    [[nodiscard]] ResultCode initializeOption() override;
 public:
-    ClientMenu();
+    ClientMenu(const std::shared_ptr<HANDLE> &pipe, std::ostream &out, std::istream &in);
     [[nodiscard]] ResultCode start() override;
+
+private:
+    [[nodiscard]] ResultCode initializeOption() override;
+    std::shared_ptr<HANDLE> _pipe;
+    std::ostream &_out;
+    std::istream &_in;
 };
