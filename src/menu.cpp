2e1dd0d1690035c1e9320d93dafeb5cc67561d8d
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-07 22:22:47 +0300

    temporarily disable menu execution result

--- src/menu.cpp
+++ src/menu.cpp
@@ -27,17 +27,20 @@ Menu::Menu()
 ResultCode Menu::start()
 {
     bool started = false;
+    // TODO add menu printing here which requires field with stream out
     while (not started or not _option->isQuitOption())
     {
         started = true;
 
         ResultCode initializeResult = initializeOption();
-        if (ResultCode::OK != initializeResult)
+        /*if (ResultCode::OK != initializeResult)
             return initializeResult;
+        */
 
         ResultCode executeResult = _option->execute();
-        if (ResultCode::OK != executeResult)
+        /*if (ResultCode::OK != executeResult)
             return executeResult;
+        */
     }
     return ResultCode::OK;
 }
