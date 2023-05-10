75d42ae87da75bfb2dedb21f0ec2f38ebc7e4d34
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 12:13:51 +0300

    declare ServerOptionRead class

--- include/menu/server_option_read.h
+++ include/menu/server_option_read.h
@@ -3,3 +3,15 @@
 //
 
 #pragma once
+
+#include "menu/server_menu.h"
+
+class ServerMenu::ServerOptionRead : public MenuOption
+{
+public:
+    ServerOptionRead(const Menu *menu);
+
+    [[nodiscard]] ResultCode execute() override;
+
+    bool isQuitOption() const override;
+};
