973167b70ceaa027439010956a0317f1e2dcd7a6
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 12:17:42 +0300

    add simple implementation for ServerMenu constructor and start method

---
+++ src/menu/server_menu.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Nikita Kasinski
+//
+
+#include "menu/server_menu.h"
+
+ServerMenu::ServerMenu() : Menu()
+{
+}
+
+ResultCode ServerMenu::start()
+{
+    return Menu::start();
+}
+
