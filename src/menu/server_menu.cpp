eec96399c7072220756261293398cd5b5dbaf277
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 13:47:35 +0300

    implement createMenuOption method in ServerMenu

--- src/menu/server_menu.cpp
+++ src/menu/server_menu.cpp
@@ -2,7 +2,11 @@
 // Created by Nikita Kasinski
 //
 
+#include <memory>
 #include "menu/server_menu.h"
+#include "menu/server_option_modify.h"
+#include "menu/server_option_quit.h"
+#include "menu/server_option_read.h"
 
 ServerMenu::ServerMenu() : Menu()
 {
@@ -16,4 +20,39 @@ ResultCode ServerMenu::start()
 bool ServerMenu::isValidOptionCode(int rawEnumValue, int rawLastEnumValue) const
 {
     return Menu::isValidOptionCode(rawEnumValue, rawLastEnumValue);
+}
+
+std::expected<std::unique_ptr<Menu::MenuOption>, ResultCode> ServerMenu::createMenuOption(int rawEnumValue) const
+{
+    if (!isValidOptionCode(rawEnumValue, static_cast<int>(ServerMenu::Options::Last)))
+    {
+        return std::unexpected(ResultCode::UnrecognizedOption);
+    }
+
+    Options serverOption = static_cast<Options>(rawEnumValue);
+
+    switch(serverOption)
+    {
+        case Options::Quit:
+        {
+            return std::make_unique<ServerOptionQuit>(this);
+            break;
+        }
+        case Options::Read:
+        {
+            return std::make_unique<ServerOptionRead>(this);
+            break;
+        }
+        case Options::Modify:
+        {
+            return std::make_unique<ServerOptionModify>(this);
+        }
+        case Options::Last:
+        {
+            // shouldn't reach here
+            return std::unexpected(ResultCode::UnreachableCodeReached);
+        }
+    }
+    // neither here
+    return std::unexpected(ResultCode::UnreachableCodeReached);
 }
