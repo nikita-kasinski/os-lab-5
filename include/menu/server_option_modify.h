2f3d5ba6047682418903dd17c46ddd0c02218b47
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 12:14:03 +0300

    declare ServerOptionModify class

--- include/menu/server_option_modify.h
+++ include/menu/server_option_modify.h
@@ -3,3 +3,15 @@
 //
 
 #pragma once
+
+#include "menu/server_menu.h"
+
+class ServerMenu::ServerOptionModify : public MenuOption
+{
+public:
+    ServerOptionModify(const Menu *menu);
+
+    [[nodiscard]] ResultCode execute() override;
+
+    bool isQuitOption() const override;
+};
