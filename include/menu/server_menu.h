4d2fffd1105c73d6ce51dcaa45fda60b9bb780c5
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 12:06:41 +0300

    declare ServerMenu class

---
+++ include/menu/server_menu.h
@@ -0,0 +1,36 @@
+//
+// Created by Nikita Kasinski
+//
+
+#pragma once
+
+#include <expected>
+#include "result_codes.h"
+#include "menu/menu.h"
+
+
+class ServerMenu : public Menu
+{
+public:
+    ServerMenu();
+
+private:
+    class ServerOptionQuit;
+    class ServerOptionRead;
+    class ServerOptionModify;
+
+    enum class Options
+    {   
+        Quit,
+        Read,
+        Modify,
+        Last,
+    };
+
+    bool isValidOptionCode(int rawEnumValue) const override;
+
+    std::expected<std::unique_ptr<MenuOption>, ResultCode> createMenuOption(int rawEnumValue) const override;
+
+    [[nodiscard]] ResultCode initializeOption() override;
+
+};
