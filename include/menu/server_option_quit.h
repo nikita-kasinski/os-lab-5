69d24fd5fdad13dce86b54b15690a589d279a1e0
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-10 12:12:50 +0300

    declare ServerOptionQuit class

--- include/menu/server_option_quit.h
+++ include/menu/server_option_quit.h
@@ -3,3 +3,15 @@
 //
 
 #pragma once
+
+#include "menu/server_menu.h"
+
+class ServerMenu::ServerOptionQuit : public MenuOption
+{
+public:
+    ServerOptionQuit(const Menu *menu);
+
+    [[nodiscard]] ResultCode execute() override;
+
+    bool isQuitOption() const override;
+};
