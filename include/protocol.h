644e4c9a2d2a7bdc9367a8bff90b9c59b9a830cb
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-05-28 16:05:42 +0300

    give protocol its own type

--- include/protocol.h
+++ include/protocol.h
@@ -7,11 +7,9 @@
 class Protocol
 {
 public:
-    static constexpr char FAILURE = 0;
-    static constexpr char SUCCESS = 1;
-    static constexpr char FINISH = 2;
-    static constexpr char QUIT = 3;
-    static constexpr char READ = 4;
-    static constexpr char MODIFY = 5;
-    static constexpr char SIZE = sizeof(std::size_t);
+    typedef std::size_t PROTOCOL;
+    static constexpr PROTOCOL FAILURE = 0;
+    static constexpr PROTOCOL SUCCESS = 1;
+    static constexpr PROTOCOL FINISH = 2;
+    static constexpr size_t SIZE = sizeof(PROTOCOL);
 };
